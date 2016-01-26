5f1a073311dcd535f59e002d17ee9b159f78f32c
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2016-01-26 10:50:19 -0600

    push to heroku

--- server/routes/data.js
+++ server/routes/data.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var pg = require('pg');
 
 console.log("In data.js");
-//var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/inorder_game_db';
+//var connectionString = 'postgres://localhost:5432/inorder_game_db';
 
 if(process.env.DATABASE_URL != undefined) {
     connectionString = process.env.DATABASE_URL + "?ssl=true";
@@ -21,7 +21,7 @@ router.get('/', function(req, res){
     var triviaArray = [];
 
     pg.connect(connectionString, function(err, client, done){
-
+        console.log(err);
         var query = client.query("SELECT id, category, difficulty, points, question, " +
             "ans1, ans2, ans3, ans4, ans5, sol1, sol2, sol3, sol4, sol5 FROM inordertable ORDER BY id ASC;");
 
